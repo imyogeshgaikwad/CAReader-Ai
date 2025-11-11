ec63f641990efc29c20986855713636be270fe7e
BEAST <imyogeshgaikwad@gmail.com>
2025-11-11 13:54:17 +0000

    Done with signup and login

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
-const listings = require("./routes/listing.js");
-const reviewRoutes = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
 
 
 
@@ -55,6 +60,14 @@ app.use(session(sessionOptions));
 app.use(flash());
 
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
         res.locals.error = req.flash("error");
@@ -62,9 +75,19 @@ app.use((req,res,next)=>{
     next()
 });
 
-app.use("/listings",listings)
-app.use("/listings/:id/reviews",reviewRoutes)
+app.use("/listings",listingRouter)
+app.use("/listings/:id/reviews",reviewRouter)
+app.use("/",userRouter);
 
+
+// app.get("/demouser",async(req,res)=>{
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username:"student"
+//     });
+//    let registeredUser = await User.register(fakeUser, "helloworld")
+//    res.send(registeredUser);
+// });
 
 // Catch-all for unknown routes
 app.use((req, res, next) => {
---
+++ routes/user.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+    res.render("users/signup.ejs");
+});
+
+router.post("/signup", wrapAsync(async (req, res, next) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        
+        // Auto-login after successful registration
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to CAReader!");
+            res.redirect("/listings");
+        });
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+}));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login", 
+    passport.authenticate("local", {
+        failureRedirect: "/login",
+        failureFlash: true
+    }), 
+    async (req, res) => {
+        req.flash("success", "Welcome back to CAReader!");
+        res.redirect("/listings");
+    }
+);
+
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "You have been logged out successfully!");
+        res.redirect("/listings");
+    });
+});
+
+module.exports = router;
